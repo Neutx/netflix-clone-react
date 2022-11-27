46aa706335963d76253feb3253c664eaa5dacb09
Neutx <dlprwz@gmail.com>
2022-11-28 03:29:34 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Nav from './components/nav/nav'
+import Header from './components/header/header';
+import Content from './components/content/content';
+
+const App = () => {
+    return <div>
+        <Nav/>
+        <Header/>
+        <Content/>
+    </div>
+};
+
+export default App;  // Exporting the function
+
---
+++ src/components/content/content.js
@@ -0,0 +1,4 @@
+import './content.css';
+export default function Content() {
+    return <div className='cotent-wrapper'>Content</div>
+}
